4dcf9746cd2d02312bc357359f6a8883689b9cec
Muhamad Fauzaan <muhamadfauzan4750@gmail.com>
2026-02-27 17:11:20 +0700

    feat: Update categories and aspirations in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,19 +53,19 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // ── Kategori ──
-        $catRuangKelas  = Category::create(['nama' => 'Ruang Kelas',   'deskripsi' => 'Aspirasi terkait kondisi ruang kelas']);
-        $catToilet      = Category::create(['nama' => 'Toilet',        'deskripsi' => 'Aspirasi terkait kebersihan dan kondisi toilet']);
-        $catLaboratorium= Category::create(['nama' => 'Laboratorium',  'deskripsi' => 'Aspirasi terkait fasilitas laboratorium']);
-        $catPerpustakaan= Category::create(['nama' => 'Perpustakaan',  'deskripsi' => 'Aspirasi terkait fasilitas perpustakaan']);
-        $catKantin      = Category::create(['nama' => 'Kantin',        'deskripsi' => 'Aspirasi terkait kantin dan makanan']);
-        $catLapangan    = Category::create(['nama' => 'Lapangan',      'deskripsi' => 'Aspirasi terkait lapangan olahraga']);
+        $catMejaKursi   = Category::create(['nama' => 'Meja & Kursi',            'deskripsi' => 'Aspirasi terkait kerusakan atau kekurangan meja dan kursi']);
+        $catElektronik  = Category::create(['nama' => 'Elektronik & Teknologi',  'deskripsi' => 'Aspirasi terkait perangkat elektronik seperti proyektor, komputer, dan AC']);
+        $catSanitasi    = Category::create(['nama' => 'Sanitasi & Kebersihan',   'deskripsi' => 'Aspirasi terkait fasilitas toilet, air bersih, dan kebersihan']);
+        $catPenerangan  = Category::create(['nama' => 'Penerangan & Listrik',    'deskripsi' => 'Aspirasi terkait lampu, instalasi listrik, dan penerangan ruangan']);
+        $catOlahraga    = Category::create(['nama' => 'Peralatan Olahraga',      'deskripsi' => 'Aspirasi terkait peralatan olahraga dan kondisi lapangan']);
+        $catPerabot     = Category::create(['nama' => 'Perabot & Inventaris',    'deskripsi' => 'Aspirasi terkait perabot sekolah lainnya seperti lemari, papan tulis, dan rak']);
 
         // ── Aspirasi ──
 
         // 1. Baru diajukan (belum ada feedback)
         $asp1 = Aspiration::create([
             'user_id'     => $siswa1->id,
-            'category_id' => $catRuangKelas->id,
+            'category_id' => $catMejaKursi->id,
             'judul'       => 'Kerusakan Meja di Kelas X-1',
             'deskripsi'   => 'Terdapat beberapa meja yang rusak di kelas X-1. Kaki meja patah dan permukaan meja sudah tidak rata. Mohon segera diperbaiki karena mengganggu proses belajar.',
             'lokasi'      => 'Gedung A Lantai 2 Kelas X-1',
@@ -75,7 +75,7 @@ class DatabaseSeeder extends Seeder
         // 2. Sedang diproses + feedback admin
         $asp2 = Aspiration::create([
             'user_id'     => $siswa2->id,
-            'category_id' => $catToilet->id,
+            'category_id' => $catSanitasi->id,
             'judul'       => 'Toilet Lantai 1 Tidak Bersih',
             'deskripsi'   => 'Toilet di lantai 1 dekat kantin kondisinya kurang bersih dan bau. Air sering tidak mengalir dengan baik.',
             'lokasi'      => 'Gedung A Lantai 1',
@@ -90,7 +90,7 @@ class DatabaseSeeder extends Seeder
         // 3. Selesai + dua feedback
         $asp3 = Aspiration::create([
             'user_id'     => $siswa3->id,
-            'category_id' => $catLaboratorium->id,
+            'category_id' => $catElektronik->id,
             'judul'       => 'AC Laboratorium Komputer Rusak',
             'deskripsi'   => 'AC di laboratorium komputer sudah tidak dingin. Membuat ruangan panas dan tidak nyaman untuk praktikum.',
             'lokasi'      => 'Gedung B Lantai 3 Lab Komputer',
@@ -110,7 +110,7 @@ class DatabaseSeeder extends Seeder
         // 4. Masih diajukan
         $asp4 = Aspiration::create([
             'user_id'     => $siswa1->id,
-            'category_id' => $catPerpustakaan->id,
+            'category_id' => $catPenerangan->id,
             'judul'       => 'Lampu Perpustakaan Mati',
             'deskripsi'   => 'Beberapa lampu di perpustakaan mati, membuat area baca menjadi gelap terutama di pojok ruangan.',
             'lokasi'      => 'Gedung C Lantai 1 Perpustakaan',
@@ -120,7 +120,7 @@ class DatabaseSeeder extends Seeder
         // 5. Ditolak + feedback alasan
         $asp5 = Aspiration::create([
             'user_id'     => $siswa2->id,
-            'category_id' => $catKantin->id,
+            'category_id' => $catPerabot->id,
             'judul'       => 'Kantin Kurang Variasi Menu',
             'deskripsi'   => 'Menu makanan di kantin sangat monoton. Mohon ditambah variasi menu yang sehat dan bergizi untuk siswa.',
             'lokasi'      => 'Kantin Sekolah',
@@ -135,7 +135,7 @@ class DatabaseSeeder extends Seeder
         // 6. Diproses + feedback
         $asp6 = Aspiration::create([
             'user_id'     => $siswa3->id,
-            'category_id' => $catLapangan->id,
+            'category_id' => $catOlahraga->id,
             'judul'       => 'Lapangan Basket Berlubang',
             'deskripsi'   => 'Terdapat beberapa lubang di lapangan basket yang berpotensi menyebabkan cedera saat berolahraga.',
             'lokasi'      => 'Lapangan Basket Sekolah',
